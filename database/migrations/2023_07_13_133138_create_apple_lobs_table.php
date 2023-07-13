5fa3b0ce4e82e5556512cddc74880f9ab500befa
Fillinor <fillinorgunio@digits.ph>
2023-07-13 13:55:34 +0800

    create migration file for apple lobs

---
+++ database/migrations/2023_07_13_133138_create_apple_lobs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAppleLobsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('apple_lobs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('apple_lob_description')->nullable();
+            $table->integer('created_by')->length(10)->unsigned()->nullable();
+            $table->integer('updated_by')->length(10)->unsigned()->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('apple_lobs');
+    }
+}
