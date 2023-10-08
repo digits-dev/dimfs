f386334cf49f560b33a9349e872206ad61443a7f
Fillinor <fillinorgunio@digits.ph>
2023-10-08 08:36:01 +0800

    create gacha sku statuses model and migration

---
+++ database/migrations/2023_10_08_001856_create_gacha_sku_statuses_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGachaSkuStatusesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('gacha_sku_statuses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('status_description')->nullable();
+            $table->enum('status', ['ACTIVE', 'INACTIVE'])->nullable()->default('ACTIVE');
+            $table->integer('created_by')->length(10)->unsigned()->nullable();
+            $table->integer('updated_by')->length(10)->unsigned()->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('gacha_sku_statuses');
+    }
+}
