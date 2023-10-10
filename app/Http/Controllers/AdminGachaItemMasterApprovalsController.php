9a06109163735fb6048de44ac5a9640bab2d1eab
Fillinor <fillinorgunio@digits.ph>
2023-10-10 20:30:19 +0800

    update index table of gacha item master and item master approvals

--- app/Http/Controllers/AdminGachaItemMasterApprovalsController.php
+++ app/Http/Controllers/AdminGachaItemMasterApprovalsController.php
@@ -42,35 +42,46 @@
 			# START COLUMNS DO NOT REMOVE THIS LINE
 			$this->col = [];
 			$this->col[] = ["label"=>"Approval Status","name"=>"approval_status"];
-			$this->col[] = ["label"=>"Digits Code","name"=>"digits_code"];
 			$this->col[] = ["label"=>"JAN Number","name"=>"jan_no"];
+			$this->col[] = ["label"=>"Digits Code","name"=>"digits_code"];
+			$this->col[] = ["label"=>"Item Number","name"=>"item_no"];
 			$this->col[] = ["label"=>"SAP Number","name"=>"sap_no"];
-			$this->col[] = ["label"=>"Item No","name"=>"item_no"];
 			$this->col[] = ["label"=>"Initial WRR Date","name"=>"initial_wrr_date"];
 			$this->col[] = ["label"=>"Latest WRR Date","name"=>"latest_wrr_date"];
 			$this->col[] = ["label"=>"Brand","name"=>"gacha_brands_id","join"=>"gacha_brands,brand_description"];
+			$this->col[] = ["label"=>"Brand Status","name"=>"gacha_brands_id","join"=>"gacha_brand_statuses,status_description","join_id"=>"id"];
 			$this->col[] = ["label"=>"SKU Status","name"=>"gacha_sku_statuses_id","join"=>"gacha_sku_statuses,status_description"];
 			$this->col[] = ["label"=>"Item Description","name"=>"item_description"];
 			$this->col[] = ['label'=>'Model','name'=>'gacha_models'];
 			$this->col[] = ['label'=>'WH Category','name'=>'gacha_wh_categories_id','join'=>'gacha_wh_categories,category_description'];
 			$this->col[] = ['label'=>'MSRP JPY','name'=>'msrp'];
 			$this->col[] = ['label'=>'Current SRP','name'=>'current_srp'];
+			$this->col[] = ['label'=>'Number of Tokens','name'=>'no_of_tokens'];
+			$this->col[] = ['label'=>'LC Per Carton','name'=>'lc_per_carton'];
+			$this->col[] = ['label'=>'LC Margin Per Carton (%)','name'=>'lc_margin_per_carton'];
+			$this->col[] = ['label'=>'LC Per PC','name'=>'lc_per_pc'];
+			$this->col[] = ['label'=>'LC Margin Per PC (%)','name'=>'lc_margin_per_pc'];
+			$this->col[] = ['label'=>'SC Per PC','name'=>'store_cost'];
+			$this->col[] = ['label'=>'SC Margin Per PC (%)','name'=>'sc_margin'];
+			$this->col[] = ['label'=>'PCS Per CTN','name'=>'pcs_ctn'];
+			$this->col[] = ['label'=>'DP Per CTN','name'=>'dp_ctn'];
+			$this->col[] = ['label'=>'PCS Per DP','name'=>'pcs_dp'];
 			$this->col[] = ['label'=>'MOQ','name'=>'moq'];
+			$this->col[] = ['label'=>'Order CTN','name'=>'no_of_ctn'];
+			$this->col[] = ['label'=>'Number of Assort','name'=>'no_of_assort'];
 			$this->col[] = ['label'=>'Country of Origin','name'=>'gacha_countries_id','join'=>'gacha_countries,country_code'];
 			$this->col[] = ['label'=>'Incoterms','name'=>'gacha_incoterms_id','join'=>'gacha_incoterms,incoterm_description'];
 			$this->col[] = ['label'=>'Currency','name'=>'currencies_id','join'=>'currencies,currency_code'];
+			$this->col[] = ['label'=>'Supplier Cost','name'=>'supplier_cost'];
 			$this->col[] = ['label'=>'UOM','name'=>'gacha_uoms_id','join'=>'gacha_uoms,uom_code'];
 			$this->col[] = ['label'=>'Inventory Type','name'=>'gacha_inventory_types_id','join'=>'gacha_inventory_types,inventory_type_description'];
 			$this->col[] = ['label'=>'Vendor Type','name'=>'gacha_vendor_types_id','join'=>'gacha_vendor_types,vendor_type_description'];
-			$this->col[] = ['label'=>'Vendor Group','name'=>'gacha_vendor_groups_id','join'=>'gacha_vendor_groups,vendor_group_description'];
+			$this->col[] = ["label"=>"Vendor Group","name"=>"gacha_vendor_groups_id","join"=>"gacha_vendor_groups,vendor_group_description"];
+			$this->col[] = ["label"=>"Vendor Group Status","name"=>"gacha_vendor_groups_id","join"=>"gacha_vendor_group_statuses,status_description","join_id"=>"id"];
 			$this->col[] = ['label'=>'Age Grade','name'=>'age_grade'];
 			$this->col[] = ['label'=>'Battery','name'=>'battery'];
-			$this->col[] = ['label'=>'Created By','name'=>'created_by','join'=>'cms_users,name'];
 			$this->col[] = ['label'=>'Created Date','name'=>'created_at'];
-			$this->col[] = ['label'=>'Updated By','name'=>'updated_by','join'=>'cms_users,name'];
 			$this->col[] = ['label'=>'Updated Date','name'=>'updated_at'];
-			$this->col[] = ['label'=>'Approved By','name'=>'approved_by','join'=>'cms_users,name'];
-			$this->col[] = ['label'=>'Approved Date','name'=>'approved_at'];
 			# END COLUMNS DO NOT REMOVE THIS LINE
 
 			# START FORM DO NOT REMOVE THIS LINE
