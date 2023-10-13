f980c5d2f27ff219c5f433020f0f4ed1751c9e06
Fillinor <fillinorgunio@digits.ph>
2023-10-13 18:28:17 +0800

    update migration file for approval status accounting of gacha item masters

--- database/migrations/2023_10_13_173555_add_approval_status_acct_in_gacha_item_masters_table.php
+++ database/migrations/2023_10_13_173555_add_approval_status_acct_in_gacha_item_masters_table.php
@@ -15,9 +15,13 @@ class AddApprovalStatusAcctInGachaItemMastersTable extends Migration
     {
         Schema::table('gacha_item_masters', function (Blueprint $table) {
             $table->integer('approval_status_acct')->length(5)->nullable()->unsigned()->after('approval_status');
+            $table->timestamp('approved_at_acct')->nullable()->after('approved_at');
+            $table->integer('approved_by_acct')->length(10)->unsigned()->nullable()->after('approved_at');
         });
         Schema::table('gacha_item_master_approvals', function (Blueprint $table) {
             $table->integer('approval_status_acct')->length(5)->nullable()->unsigned()->after('approval_status');
+            $table->timestamp('approved_at_acct')->nullable()->after('approved_at');
+            $table->integer('approved_by_acct')->length(10)->unsigned()->nullable()->after('approved_at');
         });
     }
 
@@ -30,9 +34,13 @@ class AddApprovalStatusAcctInGachaItemMastersTable extends Migration
     {
         Schema::table('gacha_item_masters', function (Blueprint $table) {
             $table->dropColumn('approval_status_acct');
+            $table->dropColumn('approved_at_acct');
+            $table->dropColumn('approved_by_acct');
         });
         Schema::table('gacha_item_master_approvals', function (Blueprint $table) {
             $table->dropColumn('approval_status_acct');
+            $table->dropColumn('approved_at_acct');
+            $table->dropColumn('approved_by_acct');
         });
     }
 }
