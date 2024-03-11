8a58a9c1172cc75089c581fa3188bd28fde150ef
Fillinor <fillinorgunio@digits.ph>
2024-03-11 10:37:08 +0800

    update config key name

--- config/privileges.php
+++ config/privileges.php
@@ -1,5 +1,5 @@
 <?php
 
 return [
-    '_edit_gacha_item_desc' => explode(',', env('EDIT_ITEM_DESC_EMAIL_PRIVS', '')),
+    'edit_gacha_item_desc' => explode(',', env('EDIT_ITEM_DESC_EMAIL_PRIVS', '')),
 ];
