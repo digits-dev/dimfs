ffdbc3fb9352e3d24db42f1105ae017fe59e2db5
Mike Rodelas <mikerodelas@digits.ph>
2024-06-07 16:08:30 +0800

    add local scope for models

--- app/VendorType.php
+++ app/VendorType.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class VendorType extends Model
 {
     protected $table = 'vendor_types';
+
+    public function scopeGetCodeById($query, $id){
+        return $query->where('id',$id)->value('vendor_type_code');
+    }
 }
