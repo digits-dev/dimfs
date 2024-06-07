2ec709281f1355fc87f7d9b05c97f07c0fea1275
Mike Rodelas <mikerodelas@digits.ph>
2024-06-07 17:03:41 +0800

    adjust brand group timestamps

--- app/Http/Controllers/AdminBrandGroupsController.php
+++ app/Http/Controllers/AdminBrandGroupsController.php
@@ -88,12 +88,14 @@
 	    }
 	    public function hook_before_add(&$postdata) {        
 	        //Your code here
-            $postdata["created_by"]=CRUDBooster::myId();
+            $postdata["created_by"] = CRUDBooster::myId();
+			$postdata['created_at'] = date('Y-m-d H:i:s');
 	    }
 		
 	    public function hook_before_edit(&$postdata,$id) {        
 	        //Your code here
-            $postdata["updated_by"]=CRUDBooster::myId();
+            $postdata["updated_by"] = CRUDBooster::myId();
+			$postdata['updated_at'] = date('Y-m-d H:i:s');
 	    }
 
 	}
